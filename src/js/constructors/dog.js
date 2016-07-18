45ed5dc1befd972b7cd612204e421da6430801c2
Chad Beach <cb832001@gmail.com>
2016-07-18 16:49:37 -0400

    update of files

--- src/js/constructors/dog.js
+++ src/js/constructors/dog.js
@@ -1,3 +1,13 @@
-function Dog () {
-  
+function Dog (options) {
+  var options = options || {};
+  this.status = "normal";
+  this.color = "black";
+  this.hungry = options.hungry;
+  if (options.hungry === undefined) {
+    this.hungry = true
+  }else{
+    this.hungry = false
+  }
 };
+
+export { Dog }
--- src/js/constructors/person.js
+++ src/js/constructors/person.js
@@ -1,3 +1,20 @@
-function Person () {
-  
+function Human () {
+  var options = options || {};
+  this.cool = true
+  this.feed = function (animal){
+    animal.hungry = false
+  }
+  this.pet = function (animal) {
+    // console.log(animal);
+    animal.status = "happy";
+    if (options.cool === undefined) {
+      this.cool = false
+    }else{
+      this.cool = true
+    }
+  };
 };
+
+// var addOne = function (x) { return x + 1; };
+
+export {Human};
